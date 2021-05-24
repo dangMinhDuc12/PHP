caf864e2c1e3cacf68b501efdb7df8dc7c26a4d5
Duc Dang <dangminhduca3@gmail.com>
2021-05-24 20:45:24 +0700

    Init PHP Repo

---
+++ math.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php 
+  
+    echo 23 + 56;
+    echo '<br>';
+    echo 23 - 56;
+    echo '<br>';
+    echo 23 * 56;
+    echo '<br>';
+    echo 23/56;
+    echo '<br>';
+    echo 5%2;
+  
+  ?>
+</body>
+</html>
---
+++ variables.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php 
+    $name = 'Duc';
+    $number = 200;
+    $htmlText = '<h2>Hello</h2>';
+    // echo json_encode($name);
+    echo $name . " " . $number . " " . $htmlText;
+  ?>
+</body>
+</html>
